ec65684bb37e1d240f9377a0e78036b13b4d10c2
slym777 <darii.dan98@gmail.com>
2022-02-04 06:12:17 +0200

    - fixed user expenses

--- src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
@@ -2,8 +2,11 @@ package com.example.expensetracker.repository;
 
 import com.example.expensetracker.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,5 +17,9 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     @Override
     @Transactional
     Expense save(Expense expense);
+
+    @Query(value = "select * from Expenses e where e.debtor_id=:userId and e.is_group_expense=false", nativeQuery = true)
+    List<Expense> getUsersPersonalExpenses(Long userId);
+
 }
 
